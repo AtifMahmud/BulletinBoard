c13c6b50f54f07b04b917673f939f9fa5d25d6b2
Alexandre Krattinger <alexandre.krattinger@epfl.ch>
2017-10-16 09:02:44 -0700

    Added support for network get in Posts
    done using the Volley API
    pretty sure it doesn't work though, I am not able to test

--- BulletinBoard/app/src/main/java/com/example/bulletinboard/Posts.java
+++ BulletinBoard/app/src/main/java/com/example/bulletinboard/Posts.java
@@ -1,6 +1,9 @@
 package com.example.bulletinboard;
 
 import java.util.ArrayList;
+import java.lang.Object;
+import org.json.*
+
 
 /**
  * Class: Posts
@@ -36,4 +39,47 @@ public class Posts {
         return postList;
     }
 
+    private void updatePostList(){
+
+        RequestQueue mRequestQueue;
+
+        // Instantiate the cache
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        Network network = new BasicNetwork(new HurlStack());
+
+        // Instantiate the RequestQueue with the cache and network.
+        mRequestQueue = new RequestQueue(cache, network);
+
+        // Start the queue
+        mRequestQueue.start();
+
+        String url = "http://104.197.33.114:8000/api/posts/all/";
+
+        JsonArrayRequest req = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        populateArray(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        mRequestQueue.add(req);
+
+    }
+
+    private void populateArray(JSONArray data){
+        postList = new ArrayList<>();
+        for(JSONObject j : data){
+            Post p = Post(j.get("title"), j.get("description"));
+            postList.add(p);
+        }
+    }
 }
