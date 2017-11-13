67425af314bacbdfad1d6cb42ec5730cb9d907aa
Atif Mahmud <atifmahmud95@gmail.com>
2017-11-12 22:16:02 -0800

    Added a register user activity

--- BulletinBoard/app/src/main/java/com/example/bulletinboard/RegisterNewUser.java
+++ BulletinBoard/app/src/main/java/com/example/bulletinboard/RegisterNewUser.java
@@ -1,5 +1,6 @@
 package com.example.bulletinboard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,5 +19,15 @@ public class RegisterNewUser extends AppCompatActivity {
         setContentView(R.layout.activity_register_new_user);
     }
 
+
+    public void registerUser(View view){
+        String name = ((EditText) findViewById(R.id.name)).getText().toString().toLowerCase();
+        String firstName = ((EditText) findViewById(R.id.FirstName)).getText().toString().toLowerCase();
+        String email = ((EditText) findViewById(R.id.email)).getText().toString().toLowerCase();
+        String phone = ((EditText) findViewById(R.id.phone)).getText().toString().toLowerCase();
+        User thisUser = new User(name, firstName, email, phone);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
 
---
+++ BulletinBoard/app/src/main/java/com/example/bulletinboard/Users.java
@@ -0,0 +1,10 @@
+package com.example.bulletinboard;
+
+/**
+ * Created by atifm on 11/12/2017.
+ */
+
+public class Users {
+
+
+}
